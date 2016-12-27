98465a665934389bdbc59387ab281d6eeb326c48
grafster <grafhama@yahoo.co.uk>
2016-12-27 13:56:57 +0000

    Update 8cell.ino

--- 8cell.ino
+++ 8cell.ino
@@ -1,39 +1,13 @@
+#include <LiquidCrystal.h>
 
- 
-//battery numbers
-//1
-#define 1gatePin 10
-#define 1highPin A0
-#define 1lowPin A1
-//2
-#define 2gatePin 10
-#define 2highPin A2
-#define 2lowPin A3
-//3
-#define 3gatePin 10
-#define 3highPin A4
-#define 3lowPin A5
-//4
-#define 4gatePin 1
-#define 4highPin A6
-#define 4lowPin A7
-//5
-#define 5gatePin 10
-#define 5highPin A8
-#define 5lowPin A9
-//6
-#define 6gatePin 10
-#define 6highPin A10
-#define 6lowPin A11
-//7
-#define 7gatePin 10
-#define 7highPin A12
-#define 7lowPin A13
-//8
-#define 8gatePin 10
-#define 8highPin A13
-#define 8lowPin A14
+#define NUM_BATTS 8 // The number of batteries.
+
+int gatePins[NUM_BATTS] = {10,10,10, 1, 10, 10, 10, 10};
+int highPins[NUM_BATTS] = {A0, A2, A4, A6, A8, A10, A12, A13};
+int lowPins[NUM_BATTS] = {A1, A3, A5, A7, A9, A11, A13, A14};
+
 // statusLED
+/*
 #define 1led
 #define 2led
 #define 3led
@@ -42,6 +16,8 @@
 #define 6led
 #define 7led
 #define 8led
+ */
+ 
  
 boolean finished = false;
 int printStart = 0;
@@ -51,8 +27,8 @@ float mAh = 0.0;
 float shuntRes = 1.0;  // In Ohms - Shunt resistor resistance
 float voltRef = 4.71; // Reference voltage (probe your 5V pin) 
 float current = 0.0;
-float 1battVolt = 0.0; float 2battVolt = 0.0; float 3battVolt = 0.0; float 4battVolt = 0.0; float 5battVolt = 0.0; float 6battVolt = 0.0; float 7battVolt = 0.0; float 8battVolt = 0.0;
-float 1shuntVolt = 0.0; float 2shuntVolt = 0.0; float 3shuntVolt = 0.0; float 4shuntVolt = 0.0; float 5shuntVolt = 0.0; float 6shuntVolt = 0.0; float 7shuntVolt = 0.0; float 8shuntVolt = 0.0;
+//float 1battVolt = 0.0; float 2battVolt = 0.0; float 3battVolt = 0.0; float 4battVolt = 0.0; float 5battVolt = 0.0; float 6battVolt = 0.0; float 7battVolt = 0.0; float 8battVolt = 0.0;
+//float 1shuntVolt = 0.0; float 2shuntVolt = 0.0; float 3shuntVolt = 0.0; float 4shuntVolt = 0.0; float 5shuntVolt = 0.0; float 6shuntVolt = 0.0; float 7shuntVolt = 0.0; float 8shuntVolt = 0.0;
 float battLow = 3.0;
  
 unsigned long previousMillis = 0;
@@ -64,26 +40,29 @@ void setup() {
   Serial.println("Battery Capacity Checker v1.1");
   Serial.println("battVolt   current     mAh");
  
-  digitalWrite(gatePin, LOW);
+  for (int i = 0; i < NUM_BATTS; i++)
+  {
+    digitalWrite(gatePins[i], LOW);
+  }
  
 
 }
  
 void loop() {
+
+  
+  
+  for (int i = 0; i < NUM_BATTS; i++)
+  {
+    int battVolt = (analogRead(highPins[i]) * voltRef) / 1024.0;
  
-  1battVolt = analogRead(1highPin) * voltRef / 1024.0;
-  2battVolt = analogRead(2highPin) * voltRef / 1024.0;
-  3battVolt = analogRead(3highPin) * voltRef / 1024.0;
-  4battVolt = analogRead(4highPin) * voltRef / 1024.0;
-  5battVolt = analogRead(5highPin) * voltRef / 1024.0;
-  6battVolt = analogRead(6highPin) * voltRef / 1024.0;
-  7battVolt = analogRead(7highPin) * voltRef / 1024.0;
-  8battVolt = analogRead(8highPin) * voltRef / 1024.0;
-  shuntVolt = analogRead(lowPin) * voltRef / 1024.0;
+  
+  
+    int shuntVolt = analogRead(lowPins[i]) * voltRef / 1024.0;
    
   if(battVolt >= battLow && finished == false)
   {
-      digitalWrite(gatePin, HIGH);
+      digitalWrite(gatePins[i], HIGH);
       millisPassed = millis() - previousMillis;
       current = (battVolt - shuntVolt) / shuntRes;
       mAh = mAh + (current * 1000.0) * (millisPassed / 3600000.0);
@@ -101,7 +80,7 @@ void loop() {
   }
   if(battVolt < battLow)
   {
-      digitalWrite(gatePin, LOW);
+      digitalWrite(gatePins[i], LOW);
       
       finished = true;
        
@@ -124,10 +103,12 @@ void loop() {
 
       delay(interval * 2);
   }
+  }
 }  
 
 
 //
+/*
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 // NOTE: Cursor Position: (CHAR, LINE) start at 0  
@@ -142,16 +123,13 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I
     
   delay(8000);  
 
+*/
 
 //
-lcd display
-batt1 batt2 batt3 batt4
-mah1  mah2  mah3  mah4 
-batt5 batt6 batt7 batt8
-mah5  mah6  mah7  mah8
+//lcd display
+//batt1 batt2 batt3 batt4
+//mah1  mah2  mah3  mah4 
+//batt5 batt6 batt7 batt8
+//mah5  mah6  mah7  mah8
 
 //
-
-
-
-
